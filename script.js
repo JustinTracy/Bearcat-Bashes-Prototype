93d465782d68a8d60cef0121098376adc76269a3
Justin Tracy <Tracyjn@mail.uc.edu>
2021-07-18 23:30:05 -0400

    Created prototype

---
+++ script.js
@@ -0,0 +1,12 @@
+const createModal = document.getElementById('create-event-modal');
+const eventModal = document.getElementById('event-modal');
+const myInput = document.getElementById('myInput');
+const createBtn = document.getElementById('createBtn');
+
+// createModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// });
+
+// eventModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// });
